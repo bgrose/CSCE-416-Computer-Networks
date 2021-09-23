6baa6c164025ad8955271f83efa74fe9761ef5c0
bgrose <47512745+bgrose@users.noreply.github.com>
2021-09-23 12:18:35 -0400

    code works

--- HttpClient.java
+++ HttpClient.java
@@ -1,33 +1,65 @@
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.InputStream;
 
 /**
- * Bradley Grose
- * Project 2
- * 9/18/2021
+ * Bradley Grose Project 2 9/18/2021
  */
+
 public class HttpClient {
-    public static void main(String[] args) {
-        try{
-            URL url = new URL(args[0]);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            InputStream input = connection.getInputStream();
-            
-            //Print Header
-            String header = connection.getHeaderField(0);
-            System.out.println(header);
-            
-            //Print Body
-            int c;
-            while((c = input.read()) != -1){
-                System.out.print((char)c);
-            }
+
+    public static void getHeadAndBody(HttpURLConnection connection, BufferedWriter writer) throws Exception {
+
+        connection.setRequestMethod("GET");
+        InputStream input = connection.getInputStream();
+
+        // Get All Header Fields
+        for (int i = 0; i < connection.getHeaderFields().size(); i++) {
+            System.out.println(connection.getHeaderField(i));
         }
-        catch(Exception e){
-            System.out.println("Error: " + e.getMessage());
+
+        // Print body
+        int c;
+        while ((c = input.read()) != -1) {
+            System.out.print((char) c);
+            writer.write((char) c);
         }
     }
+
+    public static void main(String[] args) {
+        try {
+            URL url = new URL(args[0]);
+            BufferedWriter writer = new BufferedWriter(new FileWriter("HttpClientOutput.txt"));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            // Check for redirect
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_MOVED_TEMP) {
+                System.out.println("Temporarily Redirected to: " + connection.getHeaderField("Location"));
+                writer.write("Temporarily Redirected to: " + connection.getHeaderField("Location"));
+                connection = (HttpURLConnection) new URL(connection.getHeaderField("Location")).openConnection();
+                getHeadAndBody(connection, writer);
+            } else if (connection.getResponseCode() == HttpURLConnection.HTTP_MOVED_PERM) {
+                System.out.println("Permanently Redirected to: " + connection.getHeaderField("Location"));
+                writer.write("Permanently Redirected to: " + connection.getHeaderField("Location"));
+                connection = (HttpURLConnection) new URL(connection.getHeaderField("Location")).openConnection();
+                getHeadAndBody(connection, writer);
+            } else if (connection.getResponseCode() == HttpURLConnection.HTTP_SEE_OTHER) {
+                System.out.println("See Other: " + connection.getHeaderField("Location"));
+                writer.write("See Other: " + connection.getHeaderField("Location"));
+                connection = (HttpURLConnection) new URL(connection.getHeaderField("Location")).openConnection();
+                getHeadAndBody(connection, writer);
+            } else {
+                getHeadAndBody(connection, writer);
+            }
+            writer.close();
+        }
+
+        catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+
+    }
 }
-        
