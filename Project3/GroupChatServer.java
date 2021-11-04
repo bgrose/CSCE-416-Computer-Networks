0ca9aa7c307c00c181bf4682949cc6219b5a145b
bgrose <47512745+bgrose@users.noreply.github.com>
2021-11-04 11:41:22 -0400

    GroupChat Cleint

--- Project3/GroupChatServer.java
+++ Project3/GroupChatServer.java
@@ -7,9 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Group CHat Server Code
- * Bradley Grose
- * 11/1/2021
+ * Group Chat Server Code Bradley Grose 11/1/2021
  */
 public class GroupChatServer implements Runnable {
 	// For reading messages from the keyboard
@@ -18,26 +16,42 @@ public class GroupChatServer implements Runnable {
 	// For writing messages to the socket
 	private static List<PrintWriter> toClients = new ArrayList<PrintWriter>();
 
-	// Constructor sets the reader and writer for the child thread
+	/**
+	 * Constructor for Server
+	 * @param socket Socket to read the message
+	 */
 	public GroupChatServer(Socket socket) {
 		readSocket = socket;
 	}
 
+	/**
+	 * Adds Print Writer Client to the list
+	 * @param pw PrintWriter to write to the socket for Client
+	 */
 	public static synchronized boolean addClient(PrintWriter pw) {
 		return toClients.add(pw);
 	}
 
+	/**
+	 * Removes Print Writer Client from the list
+	 * @param pw PrintWriter to write to the socket for Client
+	 */
 	public static synchronized boolean removeClient(PrintWriter pw) {
-		return(toClients.remove(pw));
+		return (toClients.remove(pw));
 	}
 
+	/**
+	 * Sends message to all clients except the one who sent it
+	 * @param msg Message to send to all clients
+	 * @param author Author of the message
+	 */
 	public static synchronized void relayMessage(String msg, PrintWriter author) {
 		for (PrintWriter pw : toClients) {
-			if(pw != author)
+			if (pw != author)
 				pw.println(msg);
 		}
 	}
-	
+
 	/**
 	 * This method is called when the thread is started.
 	 */
@@ -52,20 +66,27 @@ public class GroupChatServer implements Runnable {
 				// Read a line from the user
 				String line = fromSocketReader.readLine();
 
-				// If we get null, it means EOF, so quit
-				if (line == null)
-					break;
+				System.out.println("Received: " + line);
 
+				if (line == null || line == "^c") {
+					System.out.println("Client disconnected");
+					break;
+				}
+
+				System.out.println("Relaying message to all clients");
 				relayMessage(line, toSocketWriter);
 			}
+			System.out.println("Removing client");
 			removeClient(toSocketWriter);
+			System.out.println("Closing socket writer");
+			toSocketWriter.close();
+			System.out.println("Closing socket reader");
+			fromSocketReader.close();
+
 		} catch (Exception e) {
-			System.out.println("Error: " + e);
+			System.out.println("Calling Error " + e);
 			System.exit(1);
 		}
-
-		// End the other thread too
-		System.exit(0);
 	}
 
 	/**
@@ -82,7 +103,7 @@ public class GroupChatServer implements Runnable {
 		try {
 			// Create a server socket with the given port
 			ServerSocket servSock = new ServerSocket(Integer.parseInt(args[0]));
-			while(true) {
+			while (true) {
 				// Wait to accept another client
 				Socket sock = servSock.accept();
 				// Create a new thread for the client
